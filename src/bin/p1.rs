686754c087c134e64f178c824f587fea6cb4bafa
JafarAbdi <cafer.abdi@gmail.com>
2022-12-04 02:53:48 +0300

    Solution to P1

---
+++ src/bin/p1.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fs::read_to_string;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_to_string("./p1_input.txt")?;
+    let mut carried_food: Vec<u32> = input
+        .split("\n\n")
+        .map(|e| {
+            e.split_whitespace()
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect();
+    carried_food.sort_by(|a, b| b.cmp(a));
+    if let Some(max_calories) = carried_food.first() {
+        println!("Max calories: {max_calories}");
+    }
+    let top_3: u32 = carried_food.iter().take(3).sum();
+    println!("Top 3: {top_3}");
+    Ok(())
+}

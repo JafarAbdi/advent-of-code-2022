a2bc883f535567e356d2ab41fbd11c1a9049d6d6
JafarAbdi <cafer.abdi@gmail.com>
2022-12-05 15:56:57 +0300

    Solution to P3 & P4

---
+++ src/bin/p3.rs
@@ -0,0 +1,38 @@
+use itertools::Itertools;
+use std::error::Error;
+use std::fs;
+
+fn char_to_priority(c: char) -> u32 {
+    if c.is_uppercase() {
+        return (c as u32) - 65 + 27;
+    }
+    (c as u32) - 96
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("./p3_input.txt")?;
+    let priorities: u32 = input
+        .split('\n')
+        .map(|e| {
+            let (first_compartment, second_compartment) = e.split_at(e.len() / 2);
+            for c in first_compartment.chars() {
+                if second_compartment.contains(c) {
+                    return char_to_priority(c);
+                }
+            }
+            panic!("Unexpected input '{e}'");
+        })
+        .sum();
+    println!("P1: {priorities}");
+
+    let mut priorities = 0;
+    for (elf_1, elf_2, elf_3) in input.lines().tuples() {
+        priorities += elf_1
+            .chars()
+            .filter(|c| elf_2.contains(*c) && elf_3.contains(*c))
+            .map(char_to_priority)
+            .unique()
+            .sum::<u32>();
+    }
+    println!("P2: {priorities}");
+    Ok(())
+}
---
+++ src/bin/p4.rs
@@ -0,0 +1,61 @@
+use std::{error::Error, fs};
+
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Range {
+    lower: u32,
+    upper: u32,
+}
+
+impl Range {
+    fn contains(&self, other: &Range) -> bool {
+        self.lower <= other.lower && other.upper <= self.upper
+    }
+    fn overlaps(&self, other: &Range) -> bool {
+        self.lower <= other.upper && other.lower <= self.upper
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("./p4_input.txt")?;
+    let parsed: Vec<_> = input
+        .lines()
+        .map(|e| {
+            if let Some((range1, range2)) = e.split(',').collect_tuple() {
+                let (range1_min, range1_max) = range1.split('-').collect_tuple().unwrap();
+                let (range2_min, range2_max) = range2.split('-').collect_tuple().unwrap();
+                [
+                    Range {
+                        lower: range1_min.parse().unwrap(),
+                        upper: range1_max.parse().unwrap(),
+                    },
+                    Range {
+                        lower: range2_min.parse().unwrap(),
+                        upper: range2_max.parse().unwrap(),
+                    },
+                ]
+            } else {
+                panic!("Failed to parse an input!")
+            }
+        })
+        .collect();
+
+    let s1: u32 = parsed
+        .iter()
+        .map(|e| {
+            let [range1, range2] = e;
+            (range1.contains(range2) || range2.contains(range1)) as u32
+        })
+        .sum();
+    println!("P1: {s1}");
+    let s2: u32 = parsed
+        .iter()
+        .map(|e| {
+            let [range1, range2] = e;
+            range1.overlaps(range2) as u32
+        })
+        .sum();
+    println!("P2: {s2}");
+    Ok(())
+}
